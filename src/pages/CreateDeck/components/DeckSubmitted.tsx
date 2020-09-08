59350ec1fae92ee8088053bcb12fd4fe44442c6f
Dhadhazi <dhadhazi@gmail.com>
2020-09-08 11:31:14 +0200

    deck submit page good now

--- src/pages/CreateDeck/components/DeckSubmitted.tsx
+++ src/pages/CreateDeck/components/DeckSubmitted.tsx
@@ -3,13 +3,13 @@ import { BButton } from "../../../components/BButton";
 import { Link } from "react-router-dom";
 
 export const DeckSubmitted = () => {
-  //TODO: This should be more colorful, an image maybe
   return (
     <div className="flexbox-parent-middle-top">
       <div>
         Thank you for submitting a Questionnaire! We are soon checking it, and
-        if it is a good one, we put it together and let anyone play it!{" "}
+        if it is a good one, we put it together and let anyone play it!
       </div>
+      <img src="/images/submitted.jpg" alt="Waiting for Approval" />
       <Link to="/">
         <BButton text="Back to the main page" onClick={() => {}} />
       </Link>
